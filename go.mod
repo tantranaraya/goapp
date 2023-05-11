f0ecd3e38c55cef88de0daaa2bfea5917083424b
tantranaraya <tantra.naraya@gmail.com>
2023-05-11 16:20:47 +0700

    first release

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tantranaraya/goapp
+
+go 1.20
+
+require github.com/tantranaraya/gomodule v1.0.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tantranaraya/gomodule"
+)
+
+func main() {
+	fmt.Println(gomodule.SayHello())
+}
